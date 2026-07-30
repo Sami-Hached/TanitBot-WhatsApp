d2848b40f7d3a8722d2d16a47eb9fd56e1f228a9
Sami Hached <sami.hached@bonial.com>
2026-07-30 11:13:46 +0200

    change-prompt-in-local-testing-script

--- call_command_r.py
+++ call_command_r.py
@@ -3,7 +3,7 @@ import modal
 
 CommandR = modal.Cls.from_name("command-r-transformers", "CommandR")
 
-messages = [{"role": "user", "content": sys.argv[1]}]
+messages = [{"role": "user", "content": "Which laws protect me against hacking in Tunisia?"}]
 
 for token in CommandR().generate.remote_gen(messages):
     print(token, end="", flush=True)
